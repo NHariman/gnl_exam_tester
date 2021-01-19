932f69392f17b95965148c28cd88caa7b5950e3d
Niks Hariman <nhariman@hotmail.com>
2021-01-19 19:58:22 +0100

    fixed test.sh lol

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ gcc -Wall -Werror -Wextra get_next_line_niks.c exam_main.c -o gnl_niks
 echo "test with exam_main.c"
 ./gnl_yours < exam_main.c > yours.txt
 ./gnl_niks < exam_main.c > mine.txt
-if diff yours mine > /dev/null 2>&1
+if diff yours.txt mine.txt > /dev/null 2>&1
 then
 	echo "ERRORS, differences found in differences.txt:"
 	diff yours.txt mine.txt
@@ -19,7 +19,7 @@ fi
 echo "test with biglinelol (big file, long line without newline)"
 ./gnl_yours < biglinelol > yours.txt
 ./gnl_niks < biglinelol > mine.txt
-if diff yours mine > /dev/null 2>&1
+if diff yours.txt mine.txt > /dev/null 2>&1
 then
 	echo "ERRORS, differences found in differences.txt:"
 	diff yours.txt mine.txt
@@ -31,7 +31,7 @@ fi
 echo "test with empty file"
 ./gnl_yours < empty_file > yours.txt
 ./gnl_niks < empty_file > mine.txt
-if diff yours mine > /dev/null 2>&1
+if diff yours.txt mine.txt > /dev/null 2>&1
 then
 	echo "ERRORS, differences found in differences.txt:"
 	diff yours.txt mine.txt
